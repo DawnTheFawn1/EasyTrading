440bdd0b89ac72ec5e1308a61510655df52fd4ca
Jamal <Jamalg960@gmail.com>
2016-01-22 00:52:34 -0500

    initialized project

---
+++ src/main/java/io/github/RysingDragon/SimpleTrading/SimpleTrading.java
@@ -0,0 +1,8 @@
+package io.github.RysingDragon.SimpleTrading;
+
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(id = "simpletrading", name = "SimpleTrading", version = "0.1")
+public class SimpleTrading {
+
+}
