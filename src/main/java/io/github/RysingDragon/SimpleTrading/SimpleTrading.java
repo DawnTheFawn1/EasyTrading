bb0ac4784f55cd925d9d658b6e757db8b37ff407
Jamal <Jamalg960@gmail.com>
2016-01-22 22:21:29 -0500

    added config

--- src/main/java/io/github/RysingDragon/SimpleTrading/SimpleTrading.java
+++ src/main/java/io/github/RysingDragon/SimpleTrading/SimpleTrading.java
@@ -1,8 +1,44 @@
 package io.github.RysingDragon.SimpleTrading;
 
+import java.nio.file.Path;
+
+import org.spongepowered.api.config.ConfigDir;
+import org.spongepowered.api.config.DefaultConfig;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GamePreInitializationEvent;
 import org.spongepowered.api.plugin.Plugin;
+
+import com.google.inject.Inject;
+
+import io.github.RysingDragon.SimpleTrading.config.Config;
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
 
 @Plugin(id = "simpletrading", name = "SimpleTrading", version = "0.1")
 public class SimpleTrading {
 
+	public static SimpleTrading instance;
+	
+	@Inject
+	@DefaultConfig(sharedRoot=true)
+	private Path defaultConfig;
+	
+	@Inject
+	@DefaultConfig(sharedRoot=true)
+	private ConfigurationLoader<CommentedConfigurationNode> loader;
+	
+	@Inject
+	@ConfigDir(sharedRoot=true)
+	private Path configDir;
+	
+	@Listener
+	public void onPreInit(GamePreInitializationEvent event) {
+		instance = this;
+		Config.getConfig().setup();
+	}
+	
+	public Path getConfigPath() {
+		return defaultConfig;
+	}
+	
 }
---
+++ src/main/java/io/github/RysingDragon/SimpleTrading/config/Config.java
@@ -0,0 +1,65 @@
+package io.github.RysingDragon.SimpleTrading.config;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+import io.github.RysingDragon.SimpleTrading.SimpleTrading;
+import ninja.leaping.configurate.ConfigurationNode;
+import ninja.leaping.configurate.ConfigurationOptions;
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
+
+public class Config {
+
+	File file = new File("config/SimpleTrading.conf");
+	ConfigurationLoader<CommentedConfigurationNode> loader = HoconConfigurationLoader.builder().setFile(file).build(); 
+	CommentedConfigurationNode configNode = loader.createEmptyNode(ConfigurationOptions.defaults());
+	
+	public static Config config = new Config();
+	
+	public static Config getConfig() {
+		return config;
+	}
+	
+	public void setup() {
+		
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			load();
+			setDefault();
+			save();
+		} else {
+			load();
+		}
+		
+	}
+	
+	public void save() {
+		try {
+			loader.save(configNode);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void load() {
+		try {
+			configNode = loader.load();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void setDefault() {
+		configNode.getNode("pixelmonEnabled").setValue(false).setComment("Set whether or not to use pixelmon offers");
+	}
+	
+	
+	
+}
