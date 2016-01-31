1019aac34177f0b5a31639db765a0755b4bf6b70
Jamal <Jamalg960@gmail.com>
2016-01-31 17:38:23 -0500

    add Trade Request

---
+++ src/main/java/io/github/RysingDragon/SimpleTrading/utils/Request.java
@@ -0,0 +1,23 @@
+package io.github.RysingDragon.SimpleTrading.utils;
+
+import org.spongepowered.api.entity.living.player.Player;
+
+public class Request {
+
+	private Player sender;
+	private Player receiver;
+	
+	public Request(Player sender, Player receiver) {
+		this.sender = sender;
+		this.receiver = receiver;
+	}
+	
+	public Player getSender() {
+		return this.sender;
+	}
+	
+	public Player getReceiver() {
+		return this.receiver;
+	}
+	
+}
