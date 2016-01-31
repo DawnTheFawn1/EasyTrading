5e2878cb969f63397a451272516b510b05a16e25
Jamal <Jamalg960@gmail.com>
2016-01-31 17:39:02 -0500

    add Trade object

---
+++ src/main/java/io/github/RysingDragon/SimpleTrading/utils/Trade.java
@@ -0,0 +1,23 @@
+package io.github.RysingDragon.SimpleTrading.utils;
+
+import org.spongepowered.api.entity.living.player.Player;
+
+public class Trade {
+	
+	private Player sender;
+	private Player receiver;
+
+	public Trade(Player sender, Player receiver) {
+		this.sender = sender;
+		this.receiver = receiver;
+	}
+	
+	public Player getSender() {
+		return this.sender;
+	}
+	
+	public Player getReceiver() {
+		return this.receiver;
+	}
+	
+}
