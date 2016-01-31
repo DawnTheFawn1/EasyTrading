5bb705f8383a8c9cae6be34af21d3a9c893ac024
Jamal <Jamalg960@gmail.com>
2016-01-31 17:30:26 -0500

    trade request events

---
+++ src/main/java/io/github/RysingDragon/SimpleTrading/events/TradeAcceptEvent.java
@@ -0,0 +1,41 @@
+package io.github.RysingDragon.SimpleTrading.events;
+
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.event.Cancellable;
+import org.spongepowered.api.event.cause.Cause;
+import org.spongepowered.api.event.impl.AbstractEvent;
+
+import io.github.RysingDragon.SimpleTrading.SimpleTrading;
+
+public class TradeAcceptEvent extends AbstractEvent implements Cancellable{
+
+	private Player sender;
+	private Player receiver;
+	private boolean cancelled = false;
+	
+	public TradeAcceptEvent(Player sender, Player receiver) {
+		this.sender = sender;
+		this.receiver = receiver;
+	}
+	
+	public Cause getCause() {
+		return Cause.of(SimpleTrading.instance);
+	}
+
+	public boolean isCancelled() {
+		return this.cancelled;
+	}
+
+	public void setCancelled(boolean cancel) {
+		this.cancelled = cancel;
+	}
+
+	public Player getSender() {
+		return this.sender;
+	}
+	
+	public Player getReceiver() {
+		return this.receiver;
+	}
+	
+}
